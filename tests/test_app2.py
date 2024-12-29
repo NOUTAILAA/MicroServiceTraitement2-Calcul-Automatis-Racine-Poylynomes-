78e80e112998508be7333b47ed7fbf6208c82ab7
Ahmed Bourhazi <medbourhazi1234@gmail.com>
2024-12-29 18:37:42 +0100

    tester les piplines

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -1,10 +1,16 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app2 import app
 
 def test_valid_polynomial():
     client = app.test_client()
     response = client.post('/process_polynomial_new', json={
         "expression": "x2 + 2x",
-        "userId": "8"
+        "userId": "1"
     })
     data = response.get_json()
 
